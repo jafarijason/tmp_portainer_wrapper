6541b2a0d478caee27ca31f87a549b4d85e57a7f
Jason Jafari <me@jasonjafari.com>
2024-09-16 17:59:46 -0400

    WIP

--- src/server/portainerExpressMiddleware.ts
+++ src/server/portainerExpressMiddleware.ts
@@ -1,11 +1,32 @@
 import { Router } from "express"
 import fetch from "node-fetch"
 import jwt from "jsonwebtoken"
+import { createWriteStream, createReadStream } from "fs";
+import { pipeline } from "stream";
+import { promisify } from "util";
+import path from "path";
+import AWS from 'aws-sdk';
+import { UnprocessableEntityException } from '@nestjs/common';
+import moment from 'moment'
+
+const pipelineAsync = promisify(pipeline);
+
+interface S3BackupConfig {
+    bucket: string;
+    region: string;
+    accessKey: string;
+    secretKey: string;
+    accessPointUrl: string;
+    backupPassword: string;
+    backupRelativePath: string;
+    backupPreFix: string;
+}
 
 interface PortainerWrapperConfig {
-    portainerUrl: string
-    portainerUserName: string
-    portainerPassword: string
+    portainerUrl: string;
+    portainerUserName: string;
+    portainerPassword: string;
+    s3BackupConfig?: S3BackupConfig;
 }
 
 let portainerUrl = ""
@@ -13,8 +34,34 @@ let portainerUserName = ""
 let portainerPassword = ""
 let portainerApiToken = ""
 let portainerApiTokenPayload: any = {}
+let s3BackupConfig: S3BackupConfig
 
 const portainerExpressMiddleware = Router()
+
+// Configure AWS S3 client
+let s3Client: AWS.S3;
+
+const configureS3Client = (s3BackupConfig: S3BackupConfig) => {
+    s3Client = new AWS.S3({
+        ...(s3BackupConfig?.accessPointUrl ? {
+            endpoint: s3BackupConfig?.accessPointUrl
+        } : {}),
+        accessKeyId: s3BackupConfig.accessKey,
+        secretAccessKey: s3BackupConfig.secretKey,
+        region: s3BackupConfig.region,
+    });
+};
+
+const uploadToS3 = async (filePath: string, s3Config: S3BackupConfig) => {
+    const fileStream = createReadStream(filePath);
+    const uploadParams = {
+        Bucket: s3Config.bucket,
+        Key: `${s3Config.backupRelativePath}/${s3Config.backupPreFix}_${path.basename(filePath)}`,
+        Body: fileStream,
+    };
+    return s3Client.upload(uploadParams).promise();
+};
+
 
 portainerExpressMiddleware.get("/test", (req, res) => {
     res.send("Test endpoint is working!")
@@ -41,21 +88,77 @@ const ensurePortainerApiToken = async () => {
         }),
     })
 
-    const response = await responseRaw.json()
+    const response: any = await responseRaw.json()
     portainerApiToken = response?.jwt
     portainerApiTokenPayload = jwt.decode(portainerApiToken)
+    // console.log('new portainerApiToken with payload of generated ', portainerApiTokenPayload)
 
     return portainerApiToken
 }
 
 export const portainerExpressMiddlewareWrapper = (config: PortainerWrapperConfig) => {
-    portainerUrl = config.portainerUrl
-    portainerUserName = config.portainerUserName
-    portainerPassword = config.portainerPassword
+    portainerUrl = config.portainerUrl;
+    portainerUserName = config.portainerUserName;
+    portainerPassword = config.portainerPassword;
+    if (config?.s3BackupConfig?.accessKey) {
+        s3BackupConfig = config?.s3BackupConfig
+        configureS3Client(config.s3BackupConfig);
+    }
+
+
+    (async () => {
+        await ensurePortainerApiToken()
+    })()
 
     setInterval(async () => {
         await ensurePortainerApiToken()
-    }, 4000)
+    }, 20000)
 
     return portainerExpressMiddleware
 }
+
+
+portainerExpressMiddleware.post("/backup", async (req, res) => {
+    try {
+        if (!s3BackupConfig?.accessKey) {
+            throw new UnprocessableEntityException('s3 backup did not specified')
+        }
+        await ensurePortainerApiToken();
+
+        // Path to save the tar.gz file
+        const backupFilePath = path.join(__dirname, `${moment().toISOString()}.tar.gz`);
+
+        const backupResponse = await fetch(`${portainerUrl}/api/backup`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${portainerApiToken}`,
+            },
+            body: JSON.stringify({
+                password: req.body.backupPassword || "defaultPassword",
+            }),
+        });
+
+        if (!backupResponse.ok) {
+            return res.status(backupResponse.status).json({
+                message: "Failed to create backup",
+                status: backupResponse.statusText,
+            });
+        }
+
+        // Stream the backup content into a tar.gz file
+        const backupFileStream = createWriteStream(backupFilePath);
+        await pipelineAsync(backupResponse.body, backupFileStream);
+
+        // Upload the tar.gz file to S3
+        const uploadResult = await uploadToS3(backupFilePath, s3BackupConfig);
+        const s3FileUrl = uploadResult.Location;
+
+        // Respond with the S3 file URL
+        res.status(200).json({ message: "Backup stored in S3", fileUrl: s3FileUrl });
+
+    } catch (error) {
+        res.status(500).json({ message: "Error creating or storing backup", error });
+    }
+});
+
--- src/ui/PortainerEnvironments.tsx
+++ src/ui/PortainerEnvironments.tsx
@@ -1,3 +1,8 @@
+/** @jsxImportSource preact */
+import { h } from 'preact'; // Import Preact's `h` function for JSX rendering
+import React from 'react';
+
+
 const PortainerEnvironments = () => {
     return <h1>Env</h1>
 }
